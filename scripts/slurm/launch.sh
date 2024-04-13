94aa13c15364f3e9f5e9324cded7db09ff9b98f4
Miguel Yermo <miguel.yermo@usc.es>
2024-04-13 20:02:17 +0200

    feat: add security check before launching

--- scripts/slurm/launch.sh
+++ scripts/slurm/launch.sh
@@ -4,5 +4,10 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 for f in $SCRIPT_DIR/../../experiments/vegetation/$(whoami)/*.json
 do
-	sbatch work.sh $f
+	if [ -f $f ]; then
+		sbatch work.sh $f
+else
+    echo "File $f does not exist! Check your paths. Exiting."
+		exit 1
+fi
 done
