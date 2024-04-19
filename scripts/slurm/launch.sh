c4fd19fb28aeafb9ad9f4b5336ac8f94bb13d2b4
Miguel Yermo <miguel.yermo@usc.es>
2024-04-19 20:02:07 +0200

    feat: add verbose work name

--- scripts/slurm/launch.sh
+++ scripts/slurm/launch.sh
@@ -5,7 +5,9 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 for f in $SCRIPT_DIR/../../experiments/vegetation/$(whoami)/*.json
 do
 	if [ -f $f ]; then
-		sbatch work.sh $f
+		filename=$(basename -- "$f")
+		filename="${filename%.*}"
+		sbatch --job-name=$filename work.sh $f
 else
     echo "File $f does not exist! Check your paths. Exiting."
 		exit 1
