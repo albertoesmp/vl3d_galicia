03eb6af8bd93b944f1a68cae8112a44be1e06416
Miguel Yermo <miguel.yermo@usc.es>
2024-05-16 16:55:37 +0200

    chore: cloud assignation removing fails

--- scripts/experiment_generation/assign_clouds.py
+++ scripts/experiment_generation/assign_clouds.py
@@ -2,6 +2,7 @@ import os
 import laspy
 import numpy as np
 import json
+import glob
 
 #################
 ### Constants ###
@@ -14,23 +15,29 @@ CLOUDS_ASSIGMENT_JSON= os.path.join(EXPERIMENTS_PATH, 'clouds_assigment.json')
 # create path if it does not exist
 os.makedirs(EXPERIMENTS_PATH, exist_ok=True)
 
+# Failed point clouds not to be included in the experiments
+FAILED_CLOUDS = ['78', '20', '201', '94', '134']
+
+FAILED_CLOUDS = [os.path.join(CLOUDS_PATH, f'RGBIr_MERGE_{num}_minmaxnorm.laz') for num in FAILED_CLOUDS]
+
+# Get all files to be deal
+files = glob.glob(os.path.join(CLOUDS_PATH, 'RGBIr_MERGE_*_minmaxnorm.laz'))
+
+files = [f for f in files if f not in FAILED_CLOUDS]
 
 MAX_POINTS_PER_GROUP = 500_000_000
 
 total_num_points = 0
 
-# print all files in clouds_path
-for root, dirs, files in os.walk(CLOUDS_PATH):
-    for file in files:
-        with laspy.open(os.path.join(root, file)) as las:
-            total_num_points += las.header.point_count
+for f in files:
+    with laspy.open(f) as las:
+        total_num_points += las.header.point_count
 
 print('Total number of points: ', total_num_points)
 points_per_person = total_num_points / 3
 print('Points per person: ', points_per_person)
 
 
-# TODO: Use dictionaries instead of lists
 miguel_clouds = {'nPoints': 0, 'nGroups': 0, 'groups': {}}
 silvia_clouds = {'nPoints': 0, 'nGroups': 0, 'groups': {}}
 samuel_clouds = {'nPoints': 0, 'nGroups': 0, 'groups': {}}
@@ -38,31 +45,22 @@ samuel_clouds = {'nPoints': 0, 'nGroups': 0, 'groups': {}}
 clouds = [miguel_clouds, silvia_clouds, samuel_clouds]
 indices = [0, 0, 0]
 
-for root, dirs, files in os.walk(CLOUDS_PATH):
-    for file in files:
-        cloud_path = os.path.join(root, file)
-        with laspy.open(cloud_path) as las:
-            num_points = las.header.point_count
-
-            # Get min among the three lists
-            min_idx = np.argmin(np.array([cloud['nPoints'] for cloud in clouds]))
-
-
-            cloud = clouds[min_idx]
-            idx = indices[min_idx]
-
-            cloud['nPoints'] += num_points
-
-            if idx not in cloud['groups']:
-                cloud['groups'][idx] = {'nPoints': 0, 'cloud': []}
-
-            if cloud['groups'][idx]['nPoints'] + num_points > MAX_POINTS_PER_GROUP:
-                idx += 1
-                indices[min_idx] = idx
-                cloud['groups'][idx] = {'nPoints': 0, 'cloud': []}
-
-            cloud['groups'][idx]['nPoints'] += num_points
-            cloud['groups'][idx]['cloud'].append(cloud_path)
+for f in files:
+    with laspy.open(f) as las:
+        num_points = las.header.point_count
+        # Get min among the three lists
+        min_idx = np.argmin(np.array([cloud['nPoints'] for cloud in clouds]))
+        cloud = clouds[min_idx]
+        idx = indices[min_idx]
+        cloud['nPoints'] += num_points
+        if idx not in cloud['groups']:
+            cloud['groups'][idx] = {'nPoints': 0, 'cloud': []}
+        if cloud['groups'][idx]['nPoints'] + num_points > MAX_POINTS_PER_GROUP:
+            idx += 1
+            indices[min_idx] = idx
+            cloud['groups'][idx] = {'nPoints': 0, 'cloud': []}
+        cloud['groups'][idx]['nPoints'] += num_points
+        cloud['groups'][idx]['cloud'].append(f)
 
 clouds[0]['nGroups'] = indices[0] + 1
 clouds[1]['nGroups'] = indices[1] + 1
