cffea6b64db9c09c5ce3b33b929cacd8ef5942a4
albertoesmp <albertoesmp@gmail.com>
2024-05-21 15:58:09 +0200

    2D neighborhoods for DL are now compatible with FPS

--- src/model/deeplearn/handle/simple_dl_model_handler.py
+++ src/model/deeplearn/handle/simple_dl_model_handler.py
@@ -580,7 +580,8 @@ class SimpleDLModelHandler(DLModelHandler):
         if self.class_weight is None:
             return None
         # Handle class weight specification
-        class_weight_low = self.class_weight.lower()
+        class_weight_low = self.class_weight.lower() \
+            if isinstance(self.class_weight, str) else self.class_weight
         if class_weight_low == "auto":  # Automatic
             num_classes = getattr(self.arch, "num_classes", None)
             if num_classes is None:
